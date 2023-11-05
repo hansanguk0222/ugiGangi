7d67d239e535191990d474039fd1fb71bfcc51e1
hansanguk0222 <hansanguk0222@gmail.com>
2023-11-05 22:15:15 +0900

    .env로 변경

--- src/service/getData.ts
+++ src/service/getData.ts
@@ -1,6 +1,6 @@
 export const getData = async (mbti: string) =>
   await fetch(
-    `https://youtube.googleapis.com/youtube/v3/search?part=snippet&chart=mostPopular&maxResults=25&q=${mbti}&key=AIzaSyAd8UZBoX49bbMBbn57wzLY-Ao7jZf7tdU`,
+    `https://youtube.googleapis.com/youtube/v3/search?part=snippet&chart=mostPopular&maxResults=25&q=${mbti}&key=${process.env['NEXT_PUBLIC_YOUTUBE_KEY']}`,
   ).then(
     async (res) =>
       (await res.json()) as { items: { id: { videoId: string }; snippet: { title: string; thumbnails: { default: { url: string } } } }[] },
